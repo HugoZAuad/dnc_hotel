7f03dce193e5e55e7bef6f3c152fd658a0f4e9d8
HugoZAuad <hugozeymer@hotmail.com>
2025-06-23 21:18:26 -0300

    Feat: Excluds password in response

--- src/modules/users/user.services.ts
+++ src/modules/users/user.services.ts
@@ -4,15 +4,15 @@ import { User } from "generated/prisma/client"
 import { createUserDTO } from "./domain/dto/CreateUser.dto"
 import { updateUserDTO } from "./domain/dto/updateUser.dto"
 import * as bcrypt from "bcrypt"
+import { userSelectFields } from "../prisma/utils/userSelectFields"
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) { }
 
   async list() {
-    return await this.prisma.user.findMany()
+    return await this.prisma.user.findMany({ select: userSelectFields })
   }
-
   async show(id: number) {
     const user = await this.isIdExists(id)
     return user
@@ -20,26 +20,26 @@ export class UserService {
 
   async create(body: createUserDTO): Promise<User> {
     body.password = await this.hashPassword(body.password)
-    return await this.prisma.user.create({ data: body })
+    return await this.prisma.user.create({
+      data: body,
+      select: userSelectFields
+    })
   }
 
   async update(id: number, body: updateUserDTO) {
-
     if (body.password) {
       body.password = await this.hashPassword(body.password)
     }
-
     await this.isIdExists(id)
-    return this.prisma.user.update({ where: { id }, data: body })
+    return this.prisma.user.update({ where: { id }, data: body, select: userSelectFields })
   }
-
   async delete(id: number) {
     await this.isIdExists(id)
-    return this.prisma.user.delete({ where: { id } })
+    return this.prisma.user.delete({ where: { id }, select: userSelectFields })
   }
 
   private async isIdExists(id: number) {
-    const user = await this.prisma.user.findUnique({ where: { id } })
+    const user = await this.prisma.user.findUnique({ where: { id }, select: userSelectFields })
     if (!user) {
       throw new NotFoundException('Usuario não existe')
     }
