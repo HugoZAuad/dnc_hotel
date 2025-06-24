bd1434475ef8555dcee179e6b4a7a0232503691e
HugoZAuad <hugozeymer@hotmail.com>
2025-06-24 14:20:55 -0300

    Feat: Cors enable

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { LoggingInterceptor } from './shared/interceptors/logging.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET, PATCH, POST, DELETE'
+  })
   app.useGlobalInterceptors(new LoggingInterceptor())
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
